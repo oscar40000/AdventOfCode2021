788bfa7feb2199fcc008c06adc1e3ff269352d3b
oscar40000 <81383334+oscar40000@users.noreply.github.com>
2021-12-19 14:04:15 +0100

    day 1 done

--- src/main/kotlin/Day_1.kt
+++ src/main/kotlin/Day_1.kt
@@ -16,7 +16,7 @@ fun main(args: Array<String>) {
 
     imput.forEachLine {
 
-        if (a == 0){
+        if (a == 0) {
             print(it)
             println(" (N/A - no previous measurement)")
         } else {
@@ -33,6 +33,56 @@ fun main(args: Array<String>) {
     }
     println()
     println("The number of times a depth measurement increases: $counter")
+    println("===============================")
+    println()
+    println()
+
+
+    var listaSum = mutableListOf<Int>()
+    imput.forEachLine {
+        listaSum.add(it.toInt())
+
+    }
+
+    println("===============================")
+    for (i in 0..listaSum.lastIndex - 2) {
+
+        println("${listaSum[i] + listaSum[i + 1] + listaSum[i + 2]} = ${listaSum[i]} + ${listaSum[i + 1]} + ${listaSum[i + 2]}")
+        listaSum[i] = listaSum[i] + listaSum[i + 1] + listaSum[i + 2]
+
+
+    }
+    listaSum.removeLast()
+    listaSum.removeLast()
+    println("===============================")
+
+    a = 0
+    counter = 0
+
+    listaSum.forEach {
+        if (a == 0) {
+            print(it)
+            println(" (N/A - no previous measurement)")
+        } else {
+            print(it)
+            if (it.toInt() == a) {
+                println("(No change)")
+            }
+            if (it.toInt() > a) {
+                println("(increased)")
+                counter++
+            }
+            if (it.toInt() < a) {
+                println("(Decresed)")
+            }
+
+        }
+        a = it.toInt()
+    }
+    println()
+    println("The number of times a depth measurement increases: $counter")
+
+
 }
 
 
